6d3f82d899bc433d257d029d670ede319303e997
Cecilia <scyxw5@gmail.com>
2025-08-06 00:04:30 +0800

    Update CharacterDataView.tsx

--- nightreign-reference-notebook/src/components/CharacterDataView.tsx
+++ nightreign-reference-notebook/src/components/CharacterDataView.tsx
@@ -204,7 +204,7 @@ const DodgeFramesComparison = () => {
       },
       interaction: {
         tooltip: {
-          render: (e: any, { title, items }: any) => {
+           render: ({ title, items }: any) => {
             const total = totalFrames[title] || 0;
             return (
               <div key={title} style={{ color: 'var(--color-text-1)' }}>
